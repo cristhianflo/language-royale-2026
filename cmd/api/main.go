d9fd26f5e4d424dfa145be19cae65ff89e890ca6
Cristhian Flores <85910843+cristhianflo@users.noreply.github.com>
2026-04-17 13:40:59 -0400

    feat: new health endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,10 +5,9 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 
-	v1 := router.Group("/api/v1")
-	v1.GET("/ping", func(c *gin.Context) {
+	router.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"ok": true,
 		})
 	})
 	router.Run() // listens on 0.0.0.0:8080 by default
